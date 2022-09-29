1c9ed7f8a9740acbc0c43a3f113fe932adaddc93
ilgakulnis <101160195+zinduolis@users.noreply.github.com>
2022-09-29 02:54:38 +0000

    Added the message and timestamp collection

--- src/Waving.jsx
+++ src/Waving.jsx
@@ -5,8 +5,9 @@ import "./css/App.css";
 
 const Waving = () => {
   const [currentAccount, setCurrentAccount] = useState('');
+  const [allWaves, setAllWaves] = useState([])
   const [totalWaves, setTotalWaves] = useState(0);
-  const contractAddress = "0x9Ddd4C47B17852fB41fd8f29510bD0c4810F097E";
+  const contractAddress = "0xC9DCC7B0288FBb4fedd8915Bc1D72aD4df0a6479";
   const contractABI = abi.abi;
   
   
@@ -60,7 +61,7 @@ const Waving = () => {
         const signer = provider.getSigner();
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
         
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await wavePortalContract.wave("I am waving");
         console.log("Mining...", waveTxn.hash);
         await waveTxn.wait();
         console.log("Mined -- ", waveTxn.hash);
@@ -95,61 +96,92 @@ const Waving = () => {
     } catch (error) {
       console.log(error);
     }
-    
   } 
+
+  const getAllWaves = async () => {
+    try{
+        const {ethereum} = window;
+        if (ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner();
+          const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+          const waves = await wavePortalContract.getAllWaves();
+
+          let wavesCleaned = [];
+          waves.forEach(wave => {
+            wavesCleaned.push({
+              address: wave.waver,
+              timestamp: new Date(wave.timestamp * 1000),
+              message: wave.message
+            });
+          });
+
+          setAllWaves(wavesCleaned);
+    } else {
+      console.log("Ethereum object doesn't exist!");
+      }
+    }  
+    catch (error) {
+      console.log(error);
+    }
+  }
   
   useEffect(() => {
     checkIfWalletIsConnected().then(getWaveCount());
   }, [])
 
   return (
-    <div className="bg-black dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-      <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
-        
-        <div className="p-6 max-w-sm mx-auto bg-emerald rounded-xl shadow-xl flex items-center space-x-10">  
+  <div className="bg-black dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+    <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">        
+      <div className="p-6 max-w-sm mx-auto bg-emerald rounded-xl shadow-xl flex items-center space-x-10">  
           👋 Hey there! 
-        </div>
-        <div className="bio">
-          
         </div>
         <div className="bio">
             Connect your Ethereum wallet and wave at me from Goerli Network!
         </div>
-
         <div className="datacontainer">
-
-        {totalWaves === 0 ?
-
-        (<div className="lds-hourglass"> 
-          Loading...
-        </div>)
-        :
-        (<div className="boxCounter">
-              <p> 🌊 {totalWaves} </p>
-        </div>)
-        }
-          
-        </div>
-
-        
-
-
-        <div className="bio">
-          {currentAccount && (
-            <button className="waveButton" onClick={wave}>
-              Wave at Me
-            </button>
-          )}        
-
-  
-          {!currentAccount && (
-            <button className="waveButton" onClick={connectWallet}>
-              Connect Wallet
-            </button>     
-        )}
-        </div>
+        {
+          !currentAccount
+        ? (
+          <div className="lds-hourglass"> 
+            Loading...
+          </div>
+          )
+        : (
+          <div className="boxCounter">
+            <p> 🌊 {totalWaves} </p>
+          </div>
+          )
+        }          
       </div>
+      <div className="bio">
+          {
+            currentAccount && (
+              <button className="waveButton" onClick={wave}>
+                Wave at Me
+              </button>
+          )}        
+  
+          {
+            !currentAccount && (
+              <button className="waveButton" onClick={connectWallet}>
+                Connect Wallet
+              </button>     
+        )}
+      </div>
+
+      {allWaves.map((wave, index) => {
+        return (
+          <div key={index} style={{ backgroundColor: "OldLace", marginTop: "16px", padding: "8px"}}>
+            <div> Address: {wave.address}</div>
+            <div>Time: {wave.timestamp.toString()}</div>
+            <div>Message: {wave.message}</div>
+          </div>
+        )
+      })}
     </div>
+  </div>
   );
 }
 
